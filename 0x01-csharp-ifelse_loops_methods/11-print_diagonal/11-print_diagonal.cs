082c332af6d3164ca91c1c3c70731ac6d7e9bccc
Emma Navarro <elnavarro55@gmail.com>
2021-07-13 18:59:41 -0500

    Program that draws a diagonal line in the terminal

---
+++ 0x01-csharp-ifelse_loops_methods/11-print_diagonal/11-print_diagonal.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Line
+{
+	public static void PrintDiagonal(int length)
+	{
+		if (length > 0)
+		{
+			for (int diagonal1 = 0; diagonal1 < length; diagonal1++)
+			{
+				for (int diagonal2 = 0; diagonal2 < diagonal1; diagonal2++)
+				{
+					Console.Write(" ");
+				}
+				Console.WriteLine(@"\");
+			}
+		}
+		Console.WriteLine();
+	}
+}
