8ad96ef4bef1bc7ede88b91a53a29f3f4068bda2
Emma Navarro <elnavarro55@gmail.com>
2021-07-13 15:27:13 -0500

    Program that prints numbers from 0 to 99

---
+++ 0x01-csharp-ifelse_loops_methods/5-print_comb/5-print_comb.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Program
+{
+	static void Main(string[] args)
+	{
+		for (int number = 0; number <= 99; number++)
+		{
+			if (number != 99)
+			{
+				Console.Write("{0:D2}, ", number);
+			}
+			else
+			{
+				Console.Write("{0:D2}\n", number);
+			}
+		}
+	}
+}
