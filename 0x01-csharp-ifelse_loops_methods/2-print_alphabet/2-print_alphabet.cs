5e334207ef6a2598c2e1a74d076101befe3a3894
Emma Navarro <elnavarro55@gmail.com>
2021-07-13 14:53:13 -0500

    Program that prints the alphabet, in lowercase, not followed by a new line

---
+++ 0x01-csharp-ifelse_loops_methods/2-print_alphabet/2-print_alphabet.cs
@@ -0,0 +1,16 @@
+﻿// Program that prints the alphabet, in lowercase, not followed by a new line.
+using System;
+
+namespace _2_print_alphabet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (char alphabet = 'a'; alphabet < 'z'; alphabet++)
+            {
+                Console.Write(alphabet);
+            }
+        }
+    }
+}
