1356b45a6e4c1565f07e013dd0c32531923d8ca6
Emma Navarro <elnavarro55@gmail.com>
2021-07-14 13:05:41 -0500

    Program that retrieves an element from an array.

---
+++ 0x02-csharp-arrays_lists_dictionaries/1-element_at/1-element_at.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Array
+{
+	public static int elementAt(int[] array, int index)
+	{
+		// Returns the element at the given index
+		// If index is out of range, print Index out of range and return -1
+		if (index < 0 || index >= array.Length)
+		{
+			Console.WriteLine("Index out of range");
+			return -1;
+		}
+		else
+		{
+			return array[index];
+		}
+	}
+}
