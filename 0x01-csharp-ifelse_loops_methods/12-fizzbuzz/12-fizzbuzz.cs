680f6da248da79d5dfad9ef77e88011b448cc2b8
Emma Navarro <elnavarro55@gmail.com>
2021-07-13 20:31:05 -0500

    Program that prints the numbers from 1 to 100 separated by a space

---
+++ 0x01-csharp-ifelse_loops_methods/12-fizzbuzz/12-fizzbuzz.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Program
+{
+	static void Main(string[] args)
+	{
+
+		for (int num = 1; num <= 100; num++)
+		{
+			if (num % 15 == 0)
+				Console.Write("FizzBuzz");
+			else if (num % 3 == 0)
+				Console.Write("Fizz");
+			else if (num % 5 == 0)
+				Console.Write("Buzz");
+			else
+				Console.Write("{0}", num);
+
+			if (num < 100)
+				Console.Write(" ");
+		}
+	}
+}
