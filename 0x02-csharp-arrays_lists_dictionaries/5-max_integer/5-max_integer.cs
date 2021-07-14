83357ee7965a41b02a3623aa4d798a2bca13563e
Emma Navarro <elnavarro55@gmail.com>
2021-07-14 17:48:29 -0500

    [Modify] max integer

--- 0x02-csharp-arrays_lists_dictionaries/5-max_integer/5-max_integer.cs
+++ 0x02-csharp-arrays_lists_dictionaries/5-max_integer/5-max_integer.cs
@@ -5,9 +5,6 @@ class List
 {
 	public static int MaxInteger(List<int> myList)
 	{
-		// Returns the largest element in a given list
-		// If list is empty, print List is empty and return -1
-		// You are not allowed to use LINQ or .Max()
 		if (myList.Count == 0)
 		{
 			Console.WriteLine("List is empty");
