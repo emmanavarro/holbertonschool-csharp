a7d947d8a7286ba3ba33fa2303e500d80039386d
Emma Navarro <elnavarro55@gmail.com>
2021-07-14 13:23:16 -0500

    [Add] Method that replaces an element of an array

---
+++ 0x02-csharp-arrays_lists_dictionaries/2-replace_element/2-replace_element.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Array
+{
+	public static int[] ReplaceElement(int[] array, int index, int n)
+	{
+		// Returns the updated array
+		// If index is out of range, print Index out of range and return the unchanged array
+		if (index < 0 || index >= array.Length)
+		{
+			Console.WriteLine("Index out of range");
+			return array;
+		}
+		array[index] = n;
+		return array;
+	}
+}
