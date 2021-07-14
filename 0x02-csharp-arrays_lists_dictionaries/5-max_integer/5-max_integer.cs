28d20af2244e9a34072489006a33509fb7effd92
Emma Navarro <elnavarro55@gmail.com>
2021-07-14 17:40:44 -0500

    [Add] Method that finds the biggest integer of a list.

---
+++ 0x02-csharp-arrays_lists_dictionaries/5-max_integer/5-max_integer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+	public static int MaxInteger(List<int> myList)
+	{
+		// Returns the largest element in a given list
+		// If list is empty, print List is empty and return -1
+		// You are not allowed to use LINQ or .Max()
+		if (myList.Count == 0)
+		{
+			Console.WriteLine("List is empty");
+			return -1;
+		}
+
+		int max;
+		myList.Sort();
+		max = myList[myList.Count - 1];
+		return max;
+	}
+}
