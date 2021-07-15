45130c761413d4582b8ed89b19f0a1f82eb9e738
Emma Navarro <elnavarro55@gmail.com>
2021-07-15 14:59:21 -0500

    [Add] Unique add

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/0-unique_add/0-unique_add.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class List
+{
+	public static int Sum(List<int> myList)
+	{
+		int sum = 0;
+
+		foreach (int number in myList.Distinct())
+		{
+			sum += number;
+		}
+		return (sum);
+	}
+}
