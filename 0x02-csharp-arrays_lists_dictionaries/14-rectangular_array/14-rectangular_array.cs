5073ea789afc080b2670eff01451d58722046a44
Emma Navarro <elnavarro55@gmail.com>
2021-07-14 22:05:20 -0500

    [Add] program that creates and prints a 5 by 5 two-dimensional array and initialize index [2,2] to 1 and all other indices to 0.

---
+++ 0x02-csharp-arrays_lists_dictionaries/14-rectangular_array/14-rectangular_array.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class twoDimensionArray
+{
+	static void Main(string[] args)
+	{
+		int width = 5;
+		int height = 5;
+
+		int[,] twoDArray = new int[width, height];
+		twoDArray [2,2] = 1;
+
+		for (int i = 0; i < width; i++)
+		{
+			for (int j = 0; j < height; j++)
+			{
+				Console.Write("{0}", twoDArray[i, j]);
+				if (j < height - 1)
+					Console.Write(" ");
+			}
+			Console.WriteLine();
+		}
+	}
+}
