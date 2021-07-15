cd109b69c4f0c061aafdc5c6d69c7e71ecd1c4aa
Emma Navarro <elnavarro55@gmail.com>
2021-07-14 22:35:48 -0500

    [Add] Square matrix

---
+++ 0x02-csharp-arrays_lists_dictionaries/15-square_matrix/15-square_matrix.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class Matrix
+{
+	public static int[,] Square(int[,] myMatrix)
+	{
+		int row, column;
+
+		for (row = 0; row < myMatrix.GetLength(0); row++)
+		{
+			for (column = 0; column < myMatrix.GetLength(1); column++)
+			{
+				myMatrix[row, column] *= myMatrix[row, column];
+			}
+		}
+		return myMatrix;
+	}
+}
