f5fe89545beabf1f5ebfc9201a5819fcd15f2c53
Emma Navarro <elnavarro55@gmail.com>
2021-07-14 19:06:37 -0500

    [Add] Method that returns the number of keys in a dictionary.

---
+++ 0x02-csharp-arrays_lists_dictionaries/8-number_keys/8-number_keys.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Dictionary
+{
+	public static int NumberOfKeys(Dictionary<string, string> myDict)
+	{
+		int keys = 0;
+		foreach (var item in myDict)
+		{
+			keys += 1;
+		}
+		return keys;
+	}
+}
