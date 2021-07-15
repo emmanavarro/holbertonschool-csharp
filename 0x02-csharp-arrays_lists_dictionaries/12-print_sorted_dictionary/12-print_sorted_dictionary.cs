3b2e4a03a15599065c71ef84bb2043cb1494cdee
Emma Navarro <elnavarro55@gmail.com>
2021-07-14 20:23:47 -0500

    [Add] Method that prints a dictionary by ordered keys.

---
+++ 0x02-csharp-arrays_lists_dictionaries/12-print_sorted_dictionary/12-print_sorted_dictionary.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Dictionary
+{
+	public static void PrintSorted(Dictionary<string, string> myDict)
+	{
+		foreach (KeyValuePair<string, string> pair in myDict.OrderBy(pair => pair.Key))
+		{
+			Console.WriteLine("{0}: {1}", pair.Key, pair.Value);
+		}
+	}
+}
