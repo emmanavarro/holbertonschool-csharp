130d4fe473a15efb002a4012d8559910a0f42360
Emma Navarro <elnavarro55@gmail.com>
2021-07-14 21:30:53 -0500

    [Add] Method that returns the key with the biggest integer value in a given dictionary.

---
+++ 0x02-csharp-arrays_lists_dictionaries/13-best_score/13-best_score.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Dictionary
+{
+	public static string BestScore(Dictionary<string, int> myList)
+	{
+		if (myList.Count == 0)
+			return "None";
+
+		List<string> newList = new List<string> ();
+		foreach (KeyValuePair<string, int> pair in myList.OrderBy(pair => pair.Value))
+		{
+			newList.Add(pair.Key);
+		}
+		return newList[newList.Count - 1];
+	}
+}
