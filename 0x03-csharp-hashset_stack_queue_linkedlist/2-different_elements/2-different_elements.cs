0e78ef8c7f1443d53db2470b597c85921e07fffe
Emma Navarro <elnavarro55@gmail.com>
2021-07-15 16:35:33 -0500

    [Add] Different elements

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/2-different_elements/2-different_elements.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static List<int> DifferentElements(List<int> list1, List<int> list2)
+    {
+        List<int> uniqueElem = new List<int>();
+
+        foreach (int number in list1)
+        {
+            if (!list2.Contains(number))
+                uniqueElem.Add(number);
+        }
+
+        foreach (int number in list2)
+        {
+            if (!list1.Contains(number))
+                uniqueElem.Add(number);
+        }
+
+        uniqueElem.Sort();
+        return uniqueElem;
+    }
+}
