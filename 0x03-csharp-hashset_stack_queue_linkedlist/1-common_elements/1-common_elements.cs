65623529eab49cf93f65f8056d07be45276804a7
Emma Navarro <elnavarro55@gmail.com>
2021-07-15 15:47:40 -0500

    [Add] Common elements

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/1-common_elements/1-common_elements.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static List<int> CommonElements(List<int> list1, List<int> list2)
+    {
+        HashSet<int> numbers1 = new HashSet<int>(list1);
+        HashSet<int> numbers2 = new HashSet<int>(list2);
+
+        List<int> sum = new List<int>();
+
+        numbers1.IntersectWith(numbers2);
+
+        foreach (int number in numbers1)
+        {
+            sum.Add(number);
+        }
+        return sum;
+    }
+}
