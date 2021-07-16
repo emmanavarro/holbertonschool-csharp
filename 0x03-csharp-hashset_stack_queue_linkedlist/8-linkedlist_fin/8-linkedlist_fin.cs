61f462ae1ba07ca62dd573d652695f04160a8cc2
Emma Navarro <elnavarro55@gmail.com>
2021-07-15 21:33:00 -0500

    [Fix] separate lines of code

--- 0x03-csharp-hashset_stack_queue_linkedlist/8-linkedlist_fin/8-linkedlist_fin.cs
+++ 0x03-csharp-hashset_stack_queue_linkedlist/8-linkedlist_fin/8-linkedlist_fin.cs
@@ -6,12 +6,14 @@ class LList
     public static int FindNode(LinkedList<int> myLList, int value)
     {
         int index = 0;
+
         foreach (int item in myLList)
         {
             if (item == value)
                 return index;
             index += 1;
         }
+
         return (-1);
     }
 }
