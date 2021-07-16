bebfe662ded6c5b9e440b6d310b86447e05a89c5
Emma Navarro <elnavarro55@gmail.com>
2021-07-15 22:44:41 -0500

    [Add] Linked List insert node

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/12-linkedlist_insert/12-linkedlist_insert.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static LinkedListNode<int> Insert(LinkedList<int> myLList, int n)
+    {
+        LinkedListNode<int> newNode = new LinkedListNode<int>(n);
+        if (myLList.First == null || myLList.First.Value >= newNode.Value)
+        {
+            myLList.AddFirst(n);
+            return newNode;
+        }
+        LinkedListNode<int> actualNode = myLList.First;
+        while (actualNode.Next != null && actualNode.Next.Value < newNode.Value)
+            actualNode = actualNode.Next;
+        myLList.AddAfter(actualNode, newNode);
+        return newNode;
+    }
+}
