50381b3ed8d71d3452eb1deff2d900c5649ff0a0
Emma Navarro <elnavarro55@gmail.com>
2021-07-15 20:58:14 -0500

    [Add] Print Linked list

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/5-print_linkedlist/5-print_linkedlist.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static LinkedList<int> CreatePrint(int size)
+    {
+        LinkedList<int> myLinkedList = new LinkedList<int>();
+
+        for (int number = 0; number < size; number++)
+        {
+            myLinkedList.AddLast(number);
+            Console.WriteLine(number);
+        }
+        return myLinkedList;
+    }
+}
