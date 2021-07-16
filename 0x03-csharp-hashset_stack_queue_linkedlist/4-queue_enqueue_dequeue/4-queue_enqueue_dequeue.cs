64364aeacccb8f9d1a2a79af502a86de560db2c6
Emma Navarro <elnavarro55@gmail.com>
2021-07-15 20:39:53 -0500

    [Add] Queue - enqueue and dequeue

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/4-queue_enqueue_dequeue/4-queue_enqueue_dequeue.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+class MyQueue
+{
+    public static Queue<string> Info(Queue<string> aQueue, string newItem, string search)
+    {
+        // Print the number of items in aQueue
+        Console.WriteLine("Number of items: {0}", aQueue.Count);
+
+        // Print the item at the top of aQueue without removing it
+        if (aQueue.Count > 0)
+            Console.WriteLine("Top item: {0}", aQueue.Peek());
+        else
+            Console.WriteLine("Queue is empty");
+
+        // Add a new given item newItem to aQueue
+        aQueue.Enqueue(newItem);
+
+        // Print if aQueue contains a given item search
+        Console.WriteLine("Queue contains \"{0}\": {1}", search, aQueue.Contains(search) ? "True" : "False");
+
+        // If aQueue contains the given item search, remove all items up to and including search;
+        // otherwise, leave aQueue as is
+        if (aQueue.Contains(search))
+            while (aQueue.Dequeue() != search);
+
+        return aQueue;
+    }
+}
