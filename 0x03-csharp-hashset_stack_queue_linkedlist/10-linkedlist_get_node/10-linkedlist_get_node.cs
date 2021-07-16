0b9c4a9cae027564f623ea6fc3ba5f35f10de5d4
Emma Navarro <elnavarro55@gmail.com>
2021-07-15 22:09:37 -0500

    [Add] Linked list get a node in the nth index

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/10-linkedlist_get_node/10-linkedlist_get_node.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static int GetNode(LinkedList<int> myLList, int n)
+    {
+        int counter = 0;
+
+        foreach (int node in myLList)
+        {
+            if (counter == n)
+                return node;
+            counter += 1;
+        }
+        return (0);
+    }
+}
