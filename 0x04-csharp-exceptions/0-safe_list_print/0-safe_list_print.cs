28eccd6db94efb9996759d75981081ba871e689c
Emma Navarro <elnavarro55@gmail.com>
2021-07-26 16:13:40 -0500

    [Add] method that prints n elements of a list

---
+++ 0x04-csharp-exceptions/0-safe_list_print/0-safe_list_print.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static int SafePrint(List<int> myList, int n)
+    {
+        int count = 0;
+
+        while (count < n)
+        {
+            try
+            {
+                Console.WriteLine(myList[count]);
+                count++;
+            }
+            catch (ArgumentOutOfRangeException)
+            {
+                break;
+            }
+        }
+        return count;
+    }
+}
