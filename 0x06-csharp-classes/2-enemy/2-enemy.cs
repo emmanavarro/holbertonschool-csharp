7be488483650bfb7160c2ff0bb0653aedc4fc09e
Emma Navarro <elnavarro55@gmail.com>
2021-07-27 19:21:04 -0500

    [Add] Based on 1-enemy, write a public class Zombie that defines a zombie

---
+++ 0x06-csharp-classes/2-enemy/2-enemy.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Enemies
+{
+    /// <summary>This class create a Zombie.</summary>
+    public class Zombie
+    {
+        /// <summary>Public field that defines the zombie's health.</summary>
+        public int health;
+
+        /// <summary>Public constructor, initializes a new instance of Zombie class.</summary>
+        public Zombie()
+        {
+            health = 0;
+        }
+
+        /// <summary>Public constructor, initializes a new instance of Zombie class and checks if value is greater than 0.</summary>
+        public Zombie(int value)
+        {
+            if (value < 0)
+            {
+                throw new ArgumentException("Health must be greater than or equal to 0");
+            }
+            else
+            {
+                health = value;
+            }
+        }
+    }
+}
