022fb1f68070840e477d61378ab9106d336976f0
Emma Navarro <elnavarro55@gmail.com>
2021-08-02 18:48:10 -0500

    [Add] Task 2 files

---
+++ 0x08-csharp-inheritance/2-subclass/2-subclass.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+/// <summary> Obj class define a method to check its inheritance </summary>
+class Obj
+{
+    /// <summary>
+    /// Method to check if an object inherits from a specified class
+    /// <param name="derivedType">derived class to check</param>
+    /// <param name="baseType">base class to evaluate from</param>
+    /// <returns>True if derivedType is a subclass of baseType, else False</returns>
+    /// </summary>
+    public static bool IsOnlyASubclass(Type derivedType, Type baseType)
+    {
+        return derivedType.IsSubclassOf(baseType);
+    }
+}
