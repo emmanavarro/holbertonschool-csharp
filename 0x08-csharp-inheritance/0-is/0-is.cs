fe3cd6addbc059a8f3145096f9b22db6ce34d6e4
Emma Navarro <elnavarro55@gmail.com>
2021-08-02 18:12:56 -0500

    [Add] Task 0 files

---
+++ 0x08-csharp-inheritance/0-is/0-is.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+/// <summary>
+/// Obj class define an method to check type of object
+/// </summary>
+class Obj
+{
+    /// <summary>
+    /// Method to check if an object is of int type
+    /// <param name="obj">integer</param>
+    /// <returns>True if obj is int, else False</returns>
+    /// </summary>
+    public static bool IsOfTypeInt(object obj)
+    {
+        return obj is int;
+    }
+}
