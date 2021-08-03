f4d49894951a926229d3f3fedcf7b9637ef20363
Emma Navarro <elnavarro55@gmail.com>
2021-08-02 19:23:19 -0500

    [Add] Task 5 files

---
+++ 0x08-csharp-inheritance/5-shape/5-shape.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+/// <summary>Class that represents a geometric shape</summary>
+class Shape
+{
+    /// <summary>Method that calculates the area of a shape</summary>
+    public virtual int Area()
+    {
+        throw new NotImplementedException ("Area() is not implemented");
+    }
+}
