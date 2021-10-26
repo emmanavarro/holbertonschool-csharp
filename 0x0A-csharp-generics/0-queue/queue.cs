e01789f4bb288518c1d363e04935631674e8275b
Emma Navarro <elnavarro55@gmail.com>
2021-10-25 21:24:36 -0500

    [Add] New method that returns the Queue type

---
+++ 0x0A-csharp-generics/0-queue/queue.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+/// <summary> Class that doesn't inherit </summary>
+/// <typeparam name="T"> parameter </typeparam>
+class Queue<T> {
+
+    /// <summary>Function to check the type of Queue</summary>
+    /// <returns></returns>
+
+    public Type CheckType () {
+        return typeof (T);
+    }
+}
