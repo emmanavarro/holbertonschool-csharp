1c063dd2ccd02ef66de7de00d2a8ef91de3fa20d
Emma Navarro <elnavarro55@gmail.com>
2021-10-26 18:40:45 -0500

    [Add] Task 0. Abstract thinking

---
+++ 0x0B-csharp-interfaces/0-abstract_thinking/0-abstract_thinking.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+
+/// <summary> Abstract Class </summary>
+public abstract class Base {
+
+  /// <summary> Name of the object </summary>
+  public string name;
+
+  /// <summary> override ToString </summary>
+  /// <returns> the object name and its type </returns>
+  public override string ToString () {
+    return $"{name} is a {this.GetType()}";
+  }
+}
