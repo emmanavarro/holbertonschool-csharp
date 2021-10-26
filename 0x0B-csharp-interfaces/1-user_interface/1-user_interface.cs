17ff62f7f8f2b6651764779f72228aba968de548
Emma Navarro <elnavarro55@gmail.com>
2021-10-26 18:46:01 -0500

    [Add] Task 1. User interface

---
+++ 0x0B-csharp-interfaces/1-user_interface/1-user_interface.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+
+/// <summary> Abstract Class </summary>
+public abstract class Base {
+
+  /// <summary> Name of the object </summary>
+  public string name;
+
+  /// <summary> override ToString </summary>
+  /// <returns> the object name and its type </returns>
+  public override string ToString () {
+    return $"{name} is a {this.GetType()}";
+  }
+}
+
+interface IInteractive {
+  void Interact ();
+}
+
+interface IBreakable {
+  int durability{ 
+    get;
+    set; 
+  }
+  void Break ();
+}
+
+interface ICollectable {
+  bool isCollected { 
+    get;
+    set; 
+  }
+  void Collect ();
+}
+
+/// <summary> Class that inherits </summary>
+class TestObject : Base, IInteractive, IBreakable, ICollectable
+{
+    public int durability { get; set; }
+    public bool isCollected { get; set; }
+    public void Interact(){}
+    public void Break(){}
+    public void Collect(){}
+}
